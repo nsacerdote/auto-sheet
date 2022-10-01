0a3aff354ad8e1646d6efa862ed5e865af8f8a66
Natalio Sacerdote <nsacerdote@users.noreply.github.com>
2022-10-01 23:49:59 +0100

    Create first version of script editor

--- demo/src/components/ScriptEditor.tsx
+++ demo/src/components/ScriptEditor.tsx
@@ -1,7 +1,26 @@
 import "./ScriptEditor.scss";
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/theme-nord_dark";
+
+function onChange(newValue: string) {
+  console.log("change", newValue);
+}
 
 function ScriptEditor() {
-  return <div>ScriptEditor!</div>;
+  return (
+    <AceEditor
+      mode="text"
+      theme="nord_dark"
+      onChange={onChange}
+      setOptions={{
+        minLines: 12,
+        maxLines: 20,
+        fontSize: 16,
+        showPrintMargin: false,
+      }}
+    />
+  );
 }
 
 export default ScriptEditor;
